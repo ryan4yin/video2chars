b9d17d9453f06d2a884c1feb36efd7a845c508f8
ryan <xiaoyin_c@qq.com>
2018-05-25 17:15:40 +0800

    同步播放音乐和字符视频

--- video2chars.py
+++ video2chars.py
@@ -1,18 +1,22 @@
 # -*- coding:utf-8 -*-
+import sys
 
 import numpy as np
 import pickle
 import os
+import invoke
+from threading import Thread
 
 # 用于生成字符画的像素，越往后视觉上越明显。。这是我自己按感觉排的，你可以随意调整。写函数里效率太低，所以只好放全局了
 pixels = " .,-'`:!1+*abcdefghijklmnopqrstuvwxyz<>()\/{}[]?234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ%&@#$"
 
 
-def video2imgs(video_name, size):
+def video2imgs(video_name, size, seconds):
     """
 
     :param video_name: 字符串, 视频文件的路径
     :param size: 二元组，(宽, 高)，用于指定生成的字符画的尺寸
+    :param seconds: 指定需要解码的时长（0-seconds）
     :return: 一个 img 对象的列表，img对象实际上就是 numpy.ndarray 数组
     """
     import cv2  # 导入 opencv
@@ -22,8 +26,13 @@ def video2imgs(video_name, size):
     # 从指定文件创建一个VideoCapture对象
     cap = cv2.VideoCapture(video_name)
 
+    # 帧率
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frames_count = fps * seconds
+
+    count = 0
     # 如果cap对象已经初始化完成了，就返回true，换句话说这是一个 while true 循环
-    while cap.isOpened():
+    while cap.isOpened() and count < frames_count:
         # cap.read() 返回值介绍：
         #   ret 表示是否读取到图像
         #   frame 为图像矩阵，类型为 numpy.ndarry.
@@ -37,13 +46,15 @@ def video2imgs(video_name, size):
 
             # 分帧保存转换结果
             img_list.append(img)
+
+            count += 1
         else:
             break
 
     # 结束时要释放空间
     cap.release()
 
-    return img_list
+    return img_list, fps
 
 
 def img2chars(img):
@@ -81,7 +92,7 @@ def imgs2chars(imgs):
     return video_chars
 
 
-def play_video(video_chars):
+def play_video(video_chars, frames_rate):
     """
     播放字符视频
     :param video_chars: 字符画的列表，每个元素为一帧
@@ -108,7 +119,7 @@ def play_video(video_chars):
                 stdscr.addstr(line_i, 0, video_chars[pic_i][line_i], curses.COLOR_WHITE)
             stdscr.refresh()  # 写入后需要refresh才会立即更新界面
 
-            time.sleep(1 / 24)  # 粗略地控制播放速度。
+            time.sleep(1 / frames_rate)  # 粗略地控制播放速度。
     finally:
         # curses 使用前要初始化，用完后无论有没有异常，都要关闭
         curses.endwin()
@@ -137,10 +148,10 @@ def get_file_name(file_path):
     从文件路径中提取出不带拓展名的文件名
     """
     # 从文件路径获取文件名 _name
-    path, file_name_with_extesion = os.path.split(file_path)
+    path, file_name_with_extension = os.path.split(file_path)
 
     # 拿到文件名前缀
-    file_name, file_extern = os.path.splitext(file_name_with_extesion)
+    file_name, file_extension = os.path.splitext(file_name_with_extension)
 
     return file_name
 
@@ -152,7 +163,7 @@ def has_file(path, file_name):
     return file_name in os.listdir(path)
 
 
-def get_video_chars(video_path):
+def get_video_chars(video_path, size, seconds):
     """
     返回视频对应的字符视频
     """
@@ -161,29 +172,48 @@ def get_video_chars(video_path):
     # 如果 video_dump 已经存在于当前文件夹，就可以直接读取进来了
     if has_file(".", video_dump):
         print("发现该视频的转换缓存，直接读取")
-        video_chars = load(video_dump)
+        video_chars, fps = load(video_dump)
     else:
         print("未发现缓存，开始字符视频转换")
 
         print("开始逐帧读取")
         # 视频转字符动画
-        imgs = video2imgs(video_path, (64, 48))
+        imgs, fps = video2imgs(video_path, size, seconds)
 
         print("视频已全部转换到图像， 开始逐帧转换为字符画")
         video_chars = imgs2chars(imgs)
 
         print("转换完成，开始缓存结果")
-        # 把转换结果保存下来
-        dump(video_chars, video_dump)
+        # 把[video_chars, fps]保存下来
+        dump([video_chars, fps], video_dump)
         print("缓存完毕")
 
-    return video_chars
+    return video_chars, fps
 
 
-if __name__ == "__main__":
+def play_audio(video_path):
+    def call():
+        invoke.run(f"mpv --no-video {video_path}", hide=True, warn=True)
+
+    p = Thread(target=call)
+    p.setDaemon(True)
+    p.start()
+
+
+def main():
     # 宽，高
     size = (64, 48)
     # 视频路径，换成你自己的
-    video_path = "BadApple.mp4"
-    video_chars = get_video_chars(video_path)
-    play_video(video_chars)
+    video_path = "/home/ryan/Codes/Python/video2chars/BadApple.mp4"
+    seconds = 30  # 只转换三十秒
+    video_chars, fps = get_video_chars(video_path, size, seconds)
+
+    # 播放音轨
+    play_audio(video_path)
+
+    # 播放视频
+    play_video(video_chars, fps)
+
+
+if __name__ == "__main__":
+    main()
