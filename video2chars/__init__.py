244a7942f6220b9b41764f555b88fb991e3f5eb2
ryan4yin <xiaoyin_c@qq.com>
2019-03-29 20:07:31 +0800

    fix docs, bump version to v0.6.5

--- video2chars/__init__.py
+++ video2chars/__init__.py
@@ -7,7 +7,7 @@ from video2chars.converter import Video2Chars
 
 @click.command()
 @click.option("--chars_width", default=100, help='The width of the generated video, in characters, default to 80')
-@click.option("--fps", default=8, help='frames per second, defaults to 10')
+@click.option("--fps", default=8, help='frames per second, default to 10')
 @click.option("--pixels", default=None, type=str, help='the chars sequence used to generate character animation')
 @click.option("--t_start", default=0, help="the start time that the video needs to be converted(in seconds)")
 @click.option("--t_end", default=None, type=int, help="the end time that the video needs to be converted(in seconds)")
