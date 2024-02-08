c88989df5b67c362382af6466292ff241358560f
Ryan Yin <xiaoyin_c@qq.com>
2024-02-08 16:05:58 +0800

    fix: pillow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="video2chars",
-    version="0.6.5",
+    version="0.6.6",
     author="Ryan Yin",
     author_email="xiaoyin_c@qq.com",
     description="Convert video to character art animation.",
@@ -13,23 +13,28 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yuansuye/video2chars",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
     packages=setuptools.find_packages(),
-    install_requires=['moviepy', 'numpy', 'pillow', 'click', 'requests', 'imageio-ffmpeg'],
-
+    install_requires=[
+        "moviepy",
+        "numpy>=1.17.3",
+        "pillow<10.0.0,>=9.5.0",
+        "click",
+        "requests",
+        "imageio_ffmpeg>=0.2.0",
+    ],
     package_data={
-        "video2chars": ['DroidSansMono.ttf'],
+        "video2chars": ["DroidSansMono.ttf"],
     },
-
     entry_points={  # Optional
-        'console_scripts': [
-            'video2chars=video2chars:convert',
+        "console_scripts": [
+            "video2chars=video2chars:convert",
         ],
     },
 )
